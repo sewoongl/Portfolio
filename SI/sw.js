5d161697c6f8d6ca7bc87c23e72b4e9ba0f3ac8a
sewoongl <38034439+sewoongl@users.noreply.github.com>
2018-12-10 10:54:11 +0900

    Add files via upload
    이사님, 나 프로필 추가

--- SI/sw.js
+++ SI/sw.js
@@ -1,6 +1,58 @@
+// 이미지 클릭시 모달 on
+$("#mem1Img").on("click", function(){
+    $("#layer").css("display", "block");
+    $("#mem1").css("display", "block");
+    $("#mem1_info1").css("display", "block");
+    $("#mem1_info2").css("display", "block");
+});
+$("#mem2Img").on("click", function(){
+    $("#layer").css("display", "block");
+    $("#mem2").css("display", "block");                 
+});
+$("#mem3Img").on("click", function(){
+    $("#layer").css("display", "block");
+    $("#mem3").css("display", "block");                 
+});
+$("#mem4Img").on("click", function(){
+    $("#layer").css("display", "block");
+    $("#mem4").css("display", "block");                 
+});
+$("#mem5Img").on("click", function(){
+    $("#layer").css("display", "block");
+    $("#mem5").css("display", "block");                 
+});
+$("#mem6Img").on("click", function(){
+    $("#layer").css("display", "block");
+    $("#mem6").css("display", "block");                 
+});
+$("#mem7Img").on("click", function(){
+    $("#layer").css("display", "block");
+    $("#mem7").css("display", "block");                 
+});
+$("#mem8Img").on("click", function(){
+    $("#layer").css("display", "block");
+    $("#mem8").css("display", "block");                 
+});
+$("#mem9Img").on("click", function(){
+    $("#layer").css("display", "block");
+    $("#mem9").css("display", "block");                 
+});
+// 모달 밖 클릭시 레이어 아웃
+$("#layer").on("click", function(){
+    layerOut();
+});
 // 레이어 아웃 모듈
 function layerOut(){
     $("#layer").css("display", "none");
-    $("container").css("opacity", "1");
-    $(".pw_nm_change").css("display", "none");
+    $("#mem1").css("display", "none");
+    $("#mem2").css("display", "none");
+    $("#mem3").css("display", "none");
+    $("#mem4").css("display", "none");
+    $("#mem5").css("display", "none");
+    $("#mem6").css("display", "none");
+    $("#mem7").css("display", "none");
+    $("#mem8").css("display", "none");
+    $("#mem9").css("display", "none");
+    $("#mem1_info1").css("display", "none");
+    $("#mem1_info2").css("display", "none");
 }
